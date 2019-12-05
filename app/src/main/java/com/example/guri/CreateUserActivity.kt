e164f2d78e6fa4569373073e29a20401bdeaab1f
hlaingtinhtun <hlaingtinhtun@gigas-jp.com>
2019-12-05 18:43:54 +0900

    create user creation activity and its screen

---
+++ app/src/main/java/com/example/guri/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.guri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun createUserClicked(view: View){
+
+    }
+
+    fun generateColorClicked(view: View){
+
+    }
+}
--- app/src/main/java/com/example/guri/LoginActivity.kt
+++ app/src/main/java/com/example/guri/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.guri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View){
-
+        val createUserIntent = Intent(this,CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
