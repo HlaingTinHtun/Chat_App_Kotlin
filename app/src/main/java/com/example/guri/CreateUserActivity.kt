8ef1f64b67da89a85f20f8a0ec753c65c60eb823
hlaingtinhtun <hlaingtinhtun@gigas-jp.com>
2019-12-05 18:58:38 +0900

    generating user avatar and color

--- app/src/main/java/com/example/guri/CreateUserActivity.kt
+++ app/src/main/java/com/example/guri/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.example.guri
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profiledefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,7 +18,18 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View){
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        userAvatar = if (color == 0){
+            "light$avatar"
+        }else{
+            "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar,"drawable",packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun createUserClicked(view: View){
@@ -20,6 +37,16 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateColorClicked(view: View){
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
 
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB],1"
     }
 }
