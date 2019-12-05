5e019d7467a4824e485896bd830bca37facaef0f
hlaingtinhtun <hlaingtinhtun@gigas-jp.com>
2019-12-05 18:13:27 +0900

    setting up necessary layouts.

--- app/src/main/java/com/example/guri/MainActivity.kt
+++ app/src/main/java/com/example/guri/MainActivity.kt
@@ -13,6 +13,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,5 +43,17 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun loginBtnNavClicked(view: View){
+
+    }
+
+    fun addChannelBtnClicked(view: View){
+
+    }
+
+    fun sendMessageBtnClicked(view: View){
+
     }
 }
