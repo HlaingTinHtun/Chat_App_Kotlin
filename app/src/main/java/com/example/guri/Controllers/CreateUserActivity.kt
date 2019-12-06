078bc01a312ee48b4e684530b2c3a8e8126ccf87
hlaingtinhtun <hlaingtinhtun@gigas-jp.com>
2019-12-06 17:02:44 +0900

    polishing up the user activities

--- app/src/main/java/com/example/guri/Controllers/CreateUserActivity.kt
+++ app/src/main/java/com/example/guri/Controllers/CreateUserActivity.kt
@@ -1,13 +1,17 @@
 package com.example.guri.Controllers
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.UserData
 import android.view.View
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.guri.R
 import com.example.guri.Services.AuthService
 import com.example.guri.Services.UserDataService
+import com.example.guri.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -19,6 +23,8 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View){
@@ -36,30 +42,6 @@ class CreateUserActivity : AppCompatActivity() {
         createAvatarImageView.setImageResource(resourceId)
     }
 
-    fun createUserClicked(view: View){
-        val userName = createUserNameText.text.toString()
-        val email = createEmailText.text.toString()
-        val password = createPasswordText.text.toString()
-
-        AuthService.registerUser(this,email,password){registerSuccess->
-            if (registerSuccess){
-                AuthService.loginUser(this,email,password){loginSuccess->
-                    if (loginSuccess){
-                        AuthService.createUser(this,userName,email,userAvatar,avatarColor){createSuccess->
-                            if (createSuccess){
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
-                                finish()
-                            }
-                        }
-                    }
-                }
-            }
-
-        }
-    }
-
     fun generateColorClicked(view: View){
         val random = Random()
         val r = random.nextInt(255)
@@ -72,5 +54,57 @@ class CreateUserActivity : AppCompatActivity() {
         val savedB = b.toDouble() / 255
 
         avatarColor = "[$savedR, $savedG, $savedB],1"
+    }
+
+    fun createUserClicked(view: View){
+        enableSpinner(true)
+        val userName = createUserNameText.text.toString()
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.registerUser(this,email,password){registerSuccess->
+                if (registerSuccess){
+                    AuthService.loginUser(this,email,password){loginSuccess->
+                        if (loginSuccess){
+                            AuthService.createUser(this,userName,email,userAvatar,avatarColor){createSuccess->
+                                if (createSuccess){
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                }else{
+                                    errorToast()
+                                }
+                            }
+                        }else{
+                            errorToast()
+                        }
+                    }
+                }else{
+                    errorToast()
+                }
+
+            }
+        }else{
+            Toast.makeText(this,"Make sure everything is filled in",Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+        }
+    }
+
+    fun errorToast() {
+        Toast.makeText(this,"Something went wrong, Please try again.",Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean){
+        if (enable){
+            createSpinner.visibility = View.VISIBLE
+        }else{
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 }
--- app/src/main/java/com/example/guri/Utilities/Constants.kt
+++ app/src/main/java/com/example/guri/Utilities/Constants.kt
@@ -4,3 +4,7 @@ const val BASE_URL = "https://gurichat.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+
+//broadcast constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
